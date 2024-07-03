b07928c9473e9b0abd3a572bcdf87922b59a19dc
Kazume Nishidate <kazume.nishidate@gmail.com>
2024-07-03 16:55:10 +0900

    file out

--- initconf2/lex2pos.pl
+++ initconf2/lex2pos.pl
@@ -6,7 +6,7 @@ my $cnt=0;
 my $xscale = 100;
 my $yscale = 100;
 $file=$ARGV[0];
-open(FL1, $file) || die "I can't open QE input file. \n";
+open(FL1, "$file");
 open(POS, "> position.txt") || die "Error:$!";
 while(<FL1>) {
     
@@ -15,20 +15,20 @@ while(<FL1>) {
    $num=length($lines[1]);
    my @lifes=split(//,$lines[1]);
    for($i=0;$i<$num;$i++){
-       if($lifes[$i] eq ".") {print " 0";}
-       if($lifes[$i] eq "O") {print " 1";}
+       if($lifes[$i] eq ".") {print POS " 0";}
+       if($lifes[$i] eq "O") {print POS " 1";}
    }
    for($i=$num;$i<$xscale;$i++){
-       print " 0";
+       print POS " 0";
    }
-   print "\n";
+   print POS "\n";
    $cnt++;
 }
 
 for($j=$cnt;$j<$yscale;$j++){
     for($i=0;$i<$xscale;$i++){
-	print " 0";
+	print POS " 0";
     }
-    print "\n";
+    print POS "\n";
 }
 
